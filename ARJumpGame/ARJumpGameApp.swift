9ffd592204df2e931d28fc74fd8d41f56f940657
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2025-12-09 12:10:04 +0700

    Initial Commit

---
+++ ARJumpGame/ARJumpGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARJumpGameApp.swift
+//  ARJumpGame
+//
+//  Created by Asad on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ARJumpGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
